7786b957f7bae152bd2a463520a6ddccf088770a
Sergey Moiseev <moiseev.sergey@gmail.com>
2024-09-11 19:50:02 +0300

    Do not config non-development env with strict defaults
    This leads to defaults being set in stone, and the impossibility to assign them in `runtime.exs` for containerized env

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 import Config
 
-config :phoenix_analytics,
-  database_path: System.get_env("DUCK_PATH") || "analytics.duckdb",
-  app_domain: System.get_env("PHX_HOST") || "example.com"
+if Mix.env() == :dev do
+  config :phoenix_analytics,
+    database_path: System.get_env("DUCK_PATH") || "analytics.duckdb",
+    app_domain: System.get_env("PHX_HOST") || "example.com"
+end
 
 import_config "#{config_env()}.exs"
